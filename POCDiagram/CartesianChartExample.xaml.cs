9cbaece5e919e29d6f21854227f1ac9a7b78c1ba
aeonxberg <x.berg1994@hotmail.com>
2016-11-17 11:30:55 +0100

    PieAndBarChart

---
+++ POCDiagram/CartesianChartExample.xaml.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Windows;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.ComponentModel;
+
+namespace POCDiagram
+{
+    public partial class CartesianChartExample
+    {
+        public SeriesCollection CartesianSeriesCollection { get; set; }
+        public Axis CartesianAxis { get; set; }
+
+        private Visibility _availabilityVisibility = Visibility.Visible;
+        private Visibility _plannedVisibility = Visibility.Visible;
+
+        public CartesianChartExample()
+        {
+           NameList = new List<string> {"Fred", "Udolf", "Carl", "Kriss", "Yarick", "Otis", "Ugo"};
+
+            CartesianSeriesCollection = new SeriesCollection()
+            {
+                //Dagen per maand
+                new ColumnSeries()
+                {
+                    Title = "Beschikbaar",
+                    Values = new ChartValues<int> {25, 26, 27, 27, 28, 26, 22}, //Elk value is van een ander persoon -> Zie Axis Labels
+                    Visibility = Availability,
+                    MaxWidth = 1000,
+                    ColumnPadding = 0
+                },
+
+                new ColumnSeries()
+                {
+                    Title = "Ingepland",
+                    Values = new ChartValues<int> {20, 22, 22, 25, 25, 20, 14},
+                    Visibility = Planned,
+                    MaxWidth = 1000,
+                    ColumnPadding = 0
+                }
+
+            };
+            
+            CartesianAxis = new Axis()
+            {
+                Labels = NameList,
+                Separator = new Separator()
+                    {Step = 1}
+            };
+
+            InitializeComponent();
+        }
+
+        public Visibility Availability
+        {
+            get { return _availabilityVisibility; }
+            set
+            {
+                _availabilityVisibility = value;
+                OnPropertyChanged("Availability");
+            }
+        }
+
+        public Visibility Planned
+        {
+            get { return _plannedVisibility; }
+            set
+            {
+                _plannedVisibility = value;
+                OnPropertyChanged("Planned");
+            }
+        }
+
+        public List<string> NameList { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ POCDiagram/PieChartExample.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.ComponentModel;
+
+namespace POCDiagram
+{
+    public partial class PieChartExample : UserControl, INotifyPropertyChanged
+    {
+        // Series Series => new PieSeries<string> { "Kevin" };
+        //change
+
+        public SeriesCollection PieSeriesCollection { get; set; }
+
+        public PieChartExample()
+        {
+            PointLabel = chartPoint => $"{chartPoint.Y} ({chartPoint.Participation:P})";
+
+            PieSeriesCollection = new SeriesCollection
+            {
+                new PieSeries //Dit is een slice
+                {
+                    Title = "Juan",
+                    Values = new ChartValues<int> {7},
+                    DataLabels = true,
+                    PushOut = 5,
+                    LabelPoint = PointLabel
+                },
+                new PieSeries //Dit is een slice
+                {
+                    Title = "Maria",
+                    Values = new ChartValues<int> {3},
+                    DataLabels = true,
+                    PushOut = 5,
+                    LabelPoint = PointLabel
+                },
+                new PieSeries //Dit is een slice
+                {
+                    Title = "Pepe",
+                    Values = new ChartValues<int> {2},
+                    DataLabels = true,
+                    PushOut = 5,
+                    LabelPoint = PointLabel
+                }
+            };
+
+            InitializeComponent();
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
